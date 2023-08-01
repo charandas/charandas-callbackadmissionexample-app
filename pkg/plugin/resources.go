edc71a6956df40273c694a2dd336a53e7c8a9b77
Charandas Batra <charandas.batra@grafana.com>
2023-08-01 13:14:02 -0700

    remove extra methods

--- pkg/plugin/resources.go
+++ pkg/plugin/resources.go
@@ -11,38 +11,6 @@ import (
 	"net/http"
 )
 
-// handlePing is an example HTTP GET resource that returns a {"message": "ok"} JSON response.
-func (a *App) handlePing(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	if _, err := w.Write([]byte(`{"message": "ok"}`)); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
-// handleEcho is an example HTTP POST resource that accepts a JSON with a "message" key and
-// returns to the client whatever it is sent.
-func (a *App) handleEcho(w http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	var body struct {
-		Message string `json:"message"`
-	}
-	if err := json.NewDecoder(req.Body).Decode(&body); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	w.Header().Add("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(body); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
 // registerRoutes takes a *http.ServeMux and registers some HTTP handlers.
 func (a *App) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/k8s/admission/mutation", a.CallMutation)
@@ -111,12 +79,16 @@ func (a *App) performValidationOrMutation(w http.ResponseWriter, req *http.Reque
 				}
 			}
 		} else {
-			jsonPatch := map[string]string{
+			defaultFieldPatch := map[string]string{
 				"op":    "add",
 				"path":  "/spec/mutated_default",
 				"value": "default_value",
 			}
-			response.Patch, err = json.Marshal([]map[string]string{jsonPatch})
+
+			patches := []map[string]string{
+				defaultFieldPatch,
+			}
+			response.Patch, err = json.Marshal(patches)
 			if err != nil {
 				response.Result = &metav1.Status{
 					TypeMeta: metav1.TypeMeta{},
